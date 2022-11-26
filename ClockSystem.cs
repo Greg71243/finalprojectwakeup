818ebac18f5c61b3c807478ddf96a4c09d2b4a02
Greg71243 <90178287+Greg71243@users.noreply.github.com>
2022-11-26 20:18:32 +0700

    Add files via upload

---
+++ ClockSystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockSystem : MonoBehaviour
+{
+    public int collectionclock;
+    public List<GameObject> clocks = new List<GameObject>();
+    public GameObject pickupPanel;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ PickItem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PickItem : MonoBehaviour
+{
+    private int score = 0;
+    public Text scoreText;
+    private AudioSource audioSource;
+    public AudioClip itemSound;
+    public AudioClip completeSound;
+
+    int itemCount;
+    private void Start()
+    {
+        itemCount = GameObject.FindGameObjectsWithTag("Item").Length;
+        scoreText.text = "Clock " + score.ToString() + "/" + itemCount.ToString();
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider target)
+    {
+        if (target.gameObject.tag.Equals("Item"))
+        {
+            Destroy(target.gameObject);
+            score += 1;
+            scoreText.text = "Clock " + score.ToString() + "/" + itemCount.ToString();
+            if (score>=itemCount)
+            {
+                audioSource.PlayOneShot(completeSound);
+                StartCoroutine(NextScene1());
+            }
+            else
+            {
+                audioSource.PlayOneShot(itemSound);
+            }
+        }
+    }
+    IEnumerator NextScene1()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("BedRoom");
+    }
+
+
+}
